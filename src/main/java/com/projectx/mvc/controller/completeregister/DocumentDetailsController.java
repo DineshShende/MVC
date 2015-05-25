9953a678b321039abca9f161047d03679742d0af
DineshShende <dineshshe@gmail.com>
2015-05-25 14:52:35 +0530

    Commit With Last modified code

--- src/main/java/com/projectx/mvc/controller/completeregister/DocumentDetailsController.java
+++ src/main/java/com/projectx/mvc/controller/completeregister/DocumentDetailsController.java
@@ -1,18 +1,12 @@
 package com.projectx.mvc.controller.completeregister;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.projectx.mvc.domain.completeregister.CustomerDetailsAngDTO;
 import com.projectx.mvc.domain.completeregister.ResponseDTO;
 import com.projectx.mvc.services.completeregister.CustomerDetailsService;
 import com.projectx.mvc.services.completeregister.DocumentDetailsService;
@@ -30,7 +23,6 @@ import com.projectx.rest.domain.completeregister.DocumentDetails;
 import com.projectx.rest.domain.completeregister.DocumentKey;
 import com.projectx.rest.domain.completeregister.UpdateDocumentDTO;
 import com.projectx.rest.domain.completeregister.UpdateDocumentVerificationStatusAndRemarkDTO;
-import com.projectx.rest.domain.completeregister.VendorDetailsDTO;
 
 @Controller
 @RequestMapping(value = "/document")
